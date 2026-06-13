552861f384e9442f9ce32dc5b34e9fa1f2603194
Xenos-prog <bbarabara057@gmail.com>
2026-06-13 15:27:16 +0200

    Add files via upload

---
+++ ubersetzer.py
@@ -0,0 +1,56 @@
+import telebot
+from deep_translator import GoogleTranslator
+from langdetect import detect
+
+TOKEN = "8884880812:AAGFI09YKnkX4JwEXgPMERq513RcKErxwSo"
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(
+        message,
+        "UA><DE übersetzer ist bereit.?\n"
+        "Gib den Text für übersetzung"
+    )
+    
+@bot.message_handler(func=lambda message:True)
+def translate_text(message):
+    text = message.text
+
+    try:
+        lang = detect(text)
+
+        if lang== 'de' :
+            translated = GoogleTranslator(
+                source='de',
+                target='uk'
+            ).translate(text)
+
+            bot.reply_to(
+                message,
+                f"UK: {translated}"
+            )
+        elif lang== 'uk' :
+            translated = GoogleTranslator(
+                source='uk',
+                target='de'
+            ).translate(text)
+
+            bot.reply_to(
+                message,
+                f"DE: {translated}"
+            )
+        else:
+            bot.reply_to(
+                message,
+                f"Kann man die sprache nicht finden"
+                )
+    
+    except Exception as e:
+        bot.reply_to(
+        message,
+        f"Something is wrong: \n {e}"
+    )
+print("Bot arbeitet...")
+bot.infinity_polling()
